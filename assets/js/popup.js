733ef970668dd7da7454fc3a9453d79ff5827db6
Dhanush D <dhanushddhanushd668@gmail.com>
2026-02-11 00:02:46 +0530

    Update-12

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,18 +1,3 @@
-
-function showSuccessPopup() {
-  document.getElementById("successPopup").style.display = "flex";
-}
-
-function closeSuccessPopup() {
-  document.getElementById("successPopup").style.display = "none";
-}
-
-// Close popup when clicking outside
-window.addEventListener("click", function (e) {
-  const popup = document.getElementById("successPopup");
-  if (e.target === popup) {
-    closeSuccessPopup();
-  }
-});
+function showSuccessPopup(){document.getElementById("successPopup").style.display="flex"}function closeSuccessPopup(){document.getElementById("successPopup").style.display="none"}window.addEventListener("click",(function(e){const s=document.getElementById("successPopup");e.target===s&&closeSuccessPopup()}));
 
 
